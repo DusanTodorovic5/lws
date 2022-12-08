5181baab0ce71a27104e9259d1de7104253c43d5
Yaël Guilloux <yael.guilloux@gmail.com>
2022-12-08 08:30:15 +0100

    fix(docus): update & fix Docus (#4)
    * fix(tokens): update tokens
    
    * chore(up): update & fix

--- tokens.config.ts
+++ tokens.config.ts
@@ -1,9 +1,9 @@
 import { defineTheme } from 'pinceau'
-import colors from './design.config'
+import { primary, secondary } from './design.config'
 
 export default defineTheme({
-  colors: {
-    primary: colors.primary,
-    secondary: colors.secondary,
+  color: {
+    primary,
+    secondary,
   },
 })
